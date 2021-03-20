bfdbfe850c4f9eaec56a81952b3db9fa83143c05
samarth <samar15>
2021-03-20 15:15:03 +0530

    redirect /login page control

---
+++ src/main/java/com/shoppingSite/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.shoppingSite.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
