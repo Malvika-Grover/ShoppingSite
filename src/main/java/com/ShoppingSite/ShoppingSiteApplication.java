b8a46e0ba3303cdf2681fd16c034ce552eae0b38
Malvika-Grover <malvikagrover22@gmail.com>
2021-03-20 13:36:24 +0530

    Initial commit

--- src/main/java/com/ShoppingSite/ShoppingSiteApplication.java
+++ src/main/java/com/ShoppingSite/ShoppingSiteApplication.java
@@ -8,6 +8,7 @@ public class ShoppingSiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingSiteApplication.class, args);
+		System.out.println("Hi");
 	}
 
 }
