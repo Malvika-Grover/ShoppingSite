477e3615a84afb22b20ee9f95242febd7a64457e
samarth <samar15>
2021-03-21 21:41:45 +0530

    remove security

--- src/main/java/com/shoppingSite/config/SecurityConfiguration.java
+++ src/main/java/com/shoppingSite/config/SecurityConfiguration.java
@@ -1,5 +1,5 @@
 package com.shoppingSite.config;
-
+/*
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -40,3 +40,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter implemen
     public PasswordEncoder getPasswordEncoder(){
         return NoOpPasswordEncoder.getInstance();}
 }
+*/
